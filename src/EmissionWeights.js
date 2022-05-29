1b061e4329f21f31a0c7ebcfd02dcee9ad904efd
barree12 <47449414+barree12@users.noreply.github.com>
2022-05-29 15:43:36 +0200

    new emissions

--- src/EmissionWeights.js
+++ src/EmissionWeights.js
@@ -27,26 +27,6 @@ export default class EmissionWeights extends React.Component {
                             </div>
                         </div>
                         <div className="weight-table-column">
-                            <span className="table-title">Bonds (30%)</span>
-                            <div className="weight-table-row">
-                            <img src={ require('./logos/cvx.png').default } alt="CVX logo" className="image" />
-                            <div className="table-item-1">CVX</div>
-                            <div className="table-item-2">17.5%</div>
-                            </div>
-                            <div className="weight-table-row">
-                            <img src={ require('./logos/alcx_eth_slp.png').default } alt="ALCX-ETH SLP" className="image" />
-                            <div className="table-item-1">ALCX/ETH SLP</div>
-                            <div className="table-item-2">10%</div>
-                            </div>
-                            <div className="weight-table-row">
-                            <img src={ require('./logos/tokemak.png').default } alt="Toke" className="image" />
-                            <div className="table-item-1">Toke</div>
-                            <div className="table-item-2">2.5%</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="weight-table">
-                        <div className="weight-table-column">
                             <span className="table-title">alAsset liquidity (24%)</span>
                             <div className="weight-table-row">
                             <img src={ require('./logos/alusd.png').default } alt="alUSD" className="image" />
@@ -88,11 +68,13 @@ export default class EmissionWeights extends React.Component {
                             </div>
                         </div>
                     </div>
+                    <div className="normal-content">As per AIP-49, 30% of emissions that were previously allocated to Olympus Pro bonds have been paused and the emissions are redirected to the Alchemix Treasury.<br /></div>
                     <div className="normal-content">Latest adjustments to emissions: <a target="_blank" rel="noreferrer" href="https://snapshot.org/#/alchemixstakers.eth/proposal/0xe111e77d0bcc78d2c7c994c38ec1e915b1b50a306f9d90fa09b1e78db71eba12">
                     AIP-31</a>, <a target="_blank" rel="noreferrer" href="https://snapshot.org/#/alchemixstakers.eth/proposal/0xa1737555482930ca3687b3ed0c955f32fe204b19216290b43a77c099a41ebf85">
                     AIP-32</a>, <a target="_blank" rel="noreferrer" href="https://snapshot.org/#/alchemixstakers.eth/proposal/0xe476e3a4bc6749e3e980b2aed3db7af63e95792ff45c626d717a213dcae96e41">
                     AIP-34</a>, <a target="_blank" rel="noreferrer" href="https://snapshot.org/#/alchemixstakers.eth/proposal/0x38c2fb21a39ee061ef5b7113d3cd8b8e4954f0a0388749d62199a8d524e69b17">
-                    AIP-42</a></div>
+                    AIP-42</a>, <a target="_blank" rel="noreferrer" href="https://snapshot.org/#/alchemixstakers.eth/proposal/0xbe4447d9317aa275662d09be1e37236e7f84c57c30c598d13d0dc7f1ce06449a">
+                    AIP-49</a></div>
                 </div>
             </div>
         );
