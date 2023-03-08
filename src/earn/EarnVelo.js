2949c995ff1f1fe81790316843afa0ab64df5a5e
barree12 <47449414+barree12@users.noreply.github.com>
2023-03-08 11:42:28 +0100

    mai velo pool added

--- src/earn/EarnVelo.js
+++ src/earn/EarnVelo.js
@@ -10,7 +10,8 @@ export default class EarnVelo extends Component {
       aprStable: 0,
       aprEth: 0,
       aprOpAlUsd: 0,
-      aprOpAlEth: 0
+      aprOpAlEth: 0,
+      aprMaiAlUsd: 0
     }
   }
 
@@ -23,20 +24,20 @@ export default class EarnVelo extends Component {
     let ethPool = "0x6fd5bee1ddb4dbbb0b7368b080ab99b8ba765902";
     let opAlUsd = "0x78fa29412998acedd7728b4cf5623ee5e2f8f589";
     let opAlEth = "0x49b5c691685aaeeaaaff57ab6ccef081a165f5bb";
+    let maiAlUsd = "0x16e7e0960a74b0ea3e61118a0c0eae8f83d43d7c";
     let stableApr = 0;
     let ethApr = 0;
     let opAlUsdApr = 0;
     let opAlEthApr = 0;
+    let maiAlUsdApr = 0;
     for(let i=0;i<result.data.length;i++){
       if(result.data[i].address === stablePool) stableApr = result.data[i].apr;
       if(result.data[i].address === ethPool) ethApr = result.data[i].apr;
-      if(result.data[i].address === opAlUsd) {
-        opAlUsdApr = result.data[i].apr;
-        console.log("aaaa")
-      }
+      if(result.data[i].address === opAlUsd) opAlUsdApr = result.data[i].apr;
       if(result.data[i].address === opAlEth) opAlEthApr = result.data[i].apr;
+      if(result.data[i].address === maiAlUsd) maiAlUsdApr = result.data[i].apr;
     }
-    this.setState({ aprLoading: false, aprStable: stableApr, aprEth: ethApr, aprOpAlUsd: opAlUsdApr, aprOpAlEth: opAlEthApr })
+    this.setState({ aprLoading: false, aprStable: stableApr, aprEth: ethApr, aprOpAlUsd: opAlUsdApr, aprOpAlEth: opAlEthApr, aprMaiAlUsd: maiAlUsdApr })
     console.log(result)
   }
 
@@ -83,7 +84,53 @@ export default class EarnVelo extends Component {
               <a href="https://app.velodrome.finance/liquidity/manage?address=0xe75a3f4bf99882ad9f8aebab2115873315425d00" target="_blank" rel="noreferrer">Deposit</a>
             </div>
           </div>
+
+          <div className="earn-yield-row">
+            
+            <div className="earn-yield-strat">
+              alUSD-MAI LP
+            </div>
+            <div className="earn-yield-chain">
+              <img src={ require('../logos/op.png').default } alt="Optimism logo" className="image" />
+            </div>
+            
+            <div className="earn-yield-alasset">
+              <img src={ require('../logos/alusd.svg').default } alt="alUSD logo" className="image" />
+            </div>
+            <div className="earn-yield-reward">
+              <img src={ require('../logos/velo_round.png').default } alt="Velodrome logo" className="image" />
+            </div>
+            <div className="earn-yield-yield">
+              {this.state.aprLoading ? "0" : Math.round(this.state.aprMaiAlUsd*100)/100}%
+            </div>
+            <div className="earn-yield-link">
+              <a href="https://app.velodrome.finance/liquidity/manage?address=0x16e7e0960a74b0ea3e61118a0c0eae8f83d43d7c" target="_blank" rel="noreferrer">Deposit</a>
+            </div>
+          </div>
           
+          <div className="earn-yield-row">
+            
+            <div className="earn-yield-strat">
+              alUSD-OP LP
+            </div>
+            <div className="earn-yield-chain">
+              <img src={ require('../logos/op.png').default } alt="Optimism logo" className="image" />
+            </div>
+            
+            <div className="earn-yield-alasset">
+              <img src={ require('../logos/alusd.svg').default } alt="alUSD logo" className="image" />
+            </div>
+            <div className="earn-yield-reward">
+              <img src={ require('../logos/velo_round.png').default } alt="Velodrome logo" className="image" />
+            </div>
+            <div className="earn-yield-yield">
+              {this.state.aprLoading ? "0" : Math.round(this.state.aprOpAlUsd*100)/100}%
+            </div>
+            <div className="earn-yield-link">
+              <a href="https://app.velodrome.finance/liquidity/manage?address=0x78fa29412998acedd7728b4cf5623ee5e2f8f589" target="_blank" rel="noreferrer">Deposit</a>
+            </div>
+          </div>
+
           <div className="earn-yield-row">
             
             <div className="earn-yield-strat">
@@ -110,29 +157,6 @@ export default class EarnVelo extends Component {
           <div className="earn-yield-row">
             
             <div className="earn-yield-strat">
-              alUSD-OP LP
-            </div>
-            <div className="earn-yield-chain">
-              <img src={ require('../logos/op.png').default } alt="Optimism logo" className="image" />
-            </div>
-            
-            <div className="earn-yield-alasset">
-              <img src={ require('../logos/alusd.svg').default } alt="alUSD logo" className="image" />
-            </div>
-            <div className="earn-yield-reward">
-              <img src={ require('../logos/velo_round.png').default } alt="Velodrome logo" className="image" />
-            </div>
-            <div className="earn-yield-yield">
-              {this.state.aprLoading ? "0" : Math.round(this.state.aprOpAlUsd*100)/100}%
-            </div>
-            <div className="earn-yield-link">
-              <a href="https://app.velodrome.finance/liquidity/manage?address=0x6fd5bee1ddb4dbbb0b7368b080ab99b8ba765902" target="_blank" rel="noreferrer">Deposit</a>
-            </div>
-          </div>
-
-          <div className="earn-yield-row">
-            
-            <div className="earn-yield-strat">
               alETH-OP LP
             </div>
             <div className="earn-yield-chain">
@@ -149,7 +173,7 @@ export default class EarnVelo extends Component {
               {this.state.aprLoading ? "0" : Math.round(this.state.aprOpAlEth*100)/100}%
             </div>
             <div className="earn-yield-link">
-              <a href="https://app.velodrome.finance/liquidity/manage?address=0x6fd5bee1ddb4dbbb0b7368b080ab99b8ba765902" target="_blank" rel="noreferrer">Deposit</a>
+              <a href="https://app.velodrome.finance/liquidity/manage?address=0x49b5c691685aaeeaaaff57ab6ccef081a165f5bb" target="_blank" rel="noreferrer">Deposit</a>
             </div>
           </div>
       </div>);
