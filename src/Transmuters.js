d10086bf66f207f0821fae7484d4a35388dd1345
barree12 <47449414+barree12@users.noreply.github.com>
2024-07-08 16:26:43 +0200

    added time to transmute

--- src/Transmuters.js
+++ src/Transmuters.js
@@ -19,15 +19,21 @@ export default class Transmuters extends React.Component {
       
       Promise.all([fetch("https://api.dune.com/api/v1/query/3849355/results?api_key=VirLip4gHVafKLfK4VBzK8E9SaUaskGE").then(res => res.json()),
         fetch("https://api.dune.com/api/v1/query/3861243/results?api_key=VirLip4gHVafKLfK4VBzK8E9SaUaskGE").then(res => res.json()),
-        fetch("https://api.dune.com/api/v1/query/3862748/results?api_key=VirLip4gHVafKLfK4VBzK8E9SaUaskGE").then(res => res.json())
+        fetch("https://api.dune.com/api/v1/query/3862748/results?api_key=VirLip4gHVafKLfK4VBzK8E9SaUaskGE").then(res => res.json()),
+        fetch("https://api.dune.com/api/v1/query/3890313/results?api_key=VirLip4gHVafKLfK4VBzK8E9SaUaskGE").then(res => res.json())
         ])
-        .then(([mainnetEth, mainnetUsdc, optiEth]) => {
+        .then(([mainnetEth, mainnetUsdc, optiEth, mainnetDai]) => {
           let transmuters = { 
             mainnetEth: mainnetEth.result.rows[0].projected_yield_rate,
+            mainnetEthTime: mainnetEth.result.rows[0].time_to_transmute,
             mainnetUsdc: mainnetUsdc.result.rows[0].projected_yield_rate,
+            mainnetUsdcTime: mainnetUsdc.result.rows[0].time_to_transmute,
             optiEth: optiEth.result.rows[0].projected_yield_rate,
+            optiEthTime: optiEth.result.rows[0].time_to_transmute,
+            mainnetDai: mainnetDai.result.rows[0].projected_yield_rate,
+            mainnetDaiTime: mainnetDai.result.rows[0].time_to_transmute
           }
-          console.log(transmuters)
+          console.log(mainnetEth)
           this.setState({ transmuters: transmuters })
         })
       .catch(function(err) {
@@ -50,11 +56,12 @@ export default class Transmuters extends React.Component {
                     <div className="small-table">
                     <h3>Transmuter yield</h3>
                     {this.state.transmutersLoading ? <LoadingComponent /> :
-                    <div className="small-table-inner-9">
-                        <span className="small-table-row"></span><span></span><span className="table-text-title">APR</span>
-                        <span className="small-table-row"></span><span className="table-text-title">Mainnet ETH</span><span className="table-text-bold">{Math.round(this.state.transmuters.mainnetEth*100)/100}%</span>
-                        <span className="small-table-row"></span><span className="table-text-title">Mainnet USDC</span><span className="table-text-bold">{Math.round(this.state.transmuters.mainnetUsdc*100)/100}%</span>
-                        <span className="small-table-row"></span><span className="table-text-title">Optimism ETH</span><span className="table-text-bold">{Math.round(this.state.transmuters.optiEth*100)/100}%</span>
+                    <div className="small-table-inner-2">
+                        <span></span><span className="table-text-title">APR</span><span className="table-text-title">Time to Transmute</span>
+                        <span className="table-text-title">Mainnet ETH</span><span className="table-text-bold">{Math.round(this.state.transmuters.mainnetEth*100)/100}%</span><span className="table-text-bold">{Math.round(this.state.transmuters.mainnetEthTime*365)} days</span>
+                        <span className="table-text-title">Mainnet USDC</span><span className="table-text-bold">{Math.round(this.state.transmuters.mainnetUsdc*100)/100}%</span><span className="table-text-bold">{Math.round(this.state.transmuters.mainnetUsdcTime*365)} days</span>
+                        <span className="table-text-title">Mainnet Dai</span><span className="table-text-bold">{Math.round(this.state.transmuters.mainnetDai*100)/100}%</span><span className="table-text-bold">{Math.round(this.state.transmuters.mainnetDaiTime*365)} days</span>
+                        <span className="table-text-title">Optimism ETH</span><span className="table-text-bold">{Math.round(this.state.transmuters.optiEth*100)/100}%</span><span className="table-text-bold">{Math.round(this.state.transmuters.optiEthTime*365)} days</span>
                     </div>}
                     </div>
                 </div>
